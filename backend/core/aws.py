4275369381afb715929aa75757990e06c3e6276e
Yumeng Liu <lym20041026@gmail.com>
2026-04-18 12:59:23 -0700

    insert audio record into dynamoDB

--- backend/core/aws.py
+++ backend/core/aws.py
@@ -8,3 +8,8 @@ AWS_REGION = os.getenv("AWS_DEFAULT_REGION", "us-east-1")
 QUEUE_URL = os.getenv("SQS_URL")
 
 sqs = boto3.client("sqs", region_name=AWS_REGION)
+s3 = boto3.client("s3", region_name=AWS_REGION)
+dynamodb = boto3.resource("dynamodb", region_name=AWS_REGION)
+
+audio_book_table = dynamodb.Table("audio-books")
+book_parts = dynamodb.Table("book-parts")
