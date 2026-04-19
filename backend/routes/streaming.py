42f3cc239e101f15c11d517e64cd4ff24a6abd75
Yumeng Liu <lym20041026@gmail.com>
2026-04-18 21:09:40 -0700

    fixed streaming

--- backend/routes/streaming.py
+++ backend/routes/streaming.py
@@ -6,7 +6,7 @@ streaming_route = APIRouter(prefix="/streaming", tags=["streaming"])
 
 def get_job_record(job_id: str):
     # Validate job id
-    response = audio_book_table.get_item(Key={"job_id": job_id}, ProjectionExpression="job_id")
+    response = audio_book_table.get_item(Key={"job_id": job_id})
     if "Item" not in response:
         raise HTTPException(status_code=404, detail="Job not found")
 
@@ -28,9 +28,19 @@ async def get_job_status(job_id: str):
         "status": "in_progress" if len(parts) < total_lines else "completed",
     }
 
+@streaming_route.get("/podcasts")
+async def get_podcasts(user_id: str):
+    audio_records = audio_book_table.query(
+        IndexName="user_id_index",
+        KeyConditionExpression=Key("user_id").eq(user_id)
+    )
+
+    return audio_records.get("Items", [])
+
 @streaming_route.get("/stream-url")
 async def get_streaming_urls(job_id: str):
     audio_record = get_job_record(job_id)
+    print(audio_record)
     total_lines = audio_record["total_lines"]
 
     parts = get_parts(job_id)
@@ -38,11 +48,11 @@ async def get_streaming_urls(job_id: str):
     if len(parts) < total_lines:
         raise HTTPException(status_code=404, detail="Job not completed")
 
+
     urls = [
         s3.generate_presigned_url(
             "get_object",
-            Prefix=f"{job_id}/",
-            Params={'Bucket': AUDIO_BUCKET_NAME, 'Key': part_record["uri"]},
+            Params={'Bucket': AUDIO_BUCKET_NAME, 'Key': f"{job_id}/{part_record["uri"]}"},
         )
         for part_record in parts
     ]
